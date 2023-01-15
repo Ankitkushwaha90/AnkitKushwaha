2910fdd5247ee238ddedb7dfb9c529b7925703e3
Ankit kushwaha <119472022+Ankitkushwaha90@users.noreply.github.com>
2023-01-15 16:40:37 +0530

    Update header.js

--- header/header.js
+++ header/header.js
@@ -16,6 +16,8 @@ const headercssData = `
 			 <!---<input class="searchinput1" type="search" placeholder="search...">-->
 
 			<div class="inlineb">
+				<i class="fa-2x fab fa-youtube" href="https://www.youtube.com/channel/UCd-aCFjjMsjMwdgtDzQ53hQ" aria-hidden="true"></i>
+			
 				<i class="fa-2x fab fa-facebook" href="https://www.facebook.com/index.php?next=https%3A%2F%2Fwww.facebook.com%2Fnd%2F%3Fgroups%252F455608227799211%252Fpermalink%252F6432951830064791%252F%26aref%3D1670378960297121%26medium%3Demail%26mid%3D5ef9f42ecab52G5b07ab087174G5ef3368919ca1G96%26bcode%3D2.1670868625.AbyWLp95BbOfClSFHdA%26n_m%3Dankitkushwahahacker99109%2540gmail.com%26lloc%3Dview_on_facebook_btn%26rms%3Dv2%26irms%3Dtrue%26asc%26d"></i> 
 				<i class="fa-2x fab fa-instagram" href="https://www.instagram.com/ankit999910939393933/"></i>
 				<i class="fa-2x fab fa-twitter" href="https://twitter.com/AnkitKu92285830"></i>
